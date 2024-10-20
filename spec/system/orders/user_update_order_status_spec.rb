7b851d1a8d5ccf5d3635c61bdd8fd5c572d3f3c3
Thiago Gois <thiagogoes1011@gmail.com>
2024-10-20 16:36:45 -0300

    adicionado estoques de produto.

--- spec/system/orders/user_update_order_status_spec.rb
+++ spec/system/orders/user_update_order_status_spec.rb
@@ -9,8 +9,11 @@ describe 'Usuário informa novo status de pedido' do
                                   description: 'Galpão destinado para cargas internacionais')
     supplier = Supplier.create!(corporate_name: 'Spark Industries Brasil LTDA', brand_name: 'Spark', restration_number: '79458216100152',
                     full_address: 'Torre da Indústria, 1', city: 'Teresina', state: 'PI', email: 'vendas@spark.com.br')
-    
+    product = ProductModel.create!(supplier: supplier, name: 'Cadeira Gamer', weight: 5,
+                                   height: 100, width: 70, depth: 75, sku: 'CAD-GAMER-1234')
     order = Order.create!(user: joao, warehouse: warehouse, supplier: supplier, estimated_delivery_date: 1.day.from_now, status: :pending)
+    OrderItem.create!(order: order, product_model: product, quantity: 5)
+
     #Act
     login_as joao
     visit root_path
@@ -22,6 +25,9 @@ describe 'Usuário informa novo status de pedido' do
     expect(page).to have_content('Situação do Pedido: Entregue')
     expect(page).not_to have_button('Marcar como CANCELADO')
     expect(page).not_to have_button('Marcar como ENTREGUE')
+    expect(StockProduct.count).to eq 5
+    estoque = StockProduct.where(product_model: product, warehouse:warehouse).count
+    expect(estoque).to eq 5
   end
 
   it 'e pedido foi cancelado' do
@@ -32,8 +38,12 @@ describe 'Usuário informa novo status de pedido' do
                                   description: 'Galpão destinado para cargas internacionais')
     supplier = Supplier.create!(corporate_name: 'Spark Industries Brasil LTDA', brand_name: 'Spark', restration_number: '79458216100152',
                     full_address: 'Torre da Indústria, 1', city: 'Teresina', state: 'PI', email: 'vendas@spark.com.br')
-    
+    product = ProductModel.create!(supplier: supplier, name: 'Cadeira Gamer', weight: 5,
+                                   height: 100, width: 70, depth: 75, sku: 'CAD-GAMER-1234')
+
     order = Order.create!(user: joao, warehouse: warehouse, supplier: supplier, estimated_delivery_date: 1.day.from_now, status: :pending)
+    OrderItem.create!(order: order, product_model: product, quantity: 5)
+    
     #Act
     login_as joao
     visit root_path
@@ -43,5 +53,6 @@ describe 'Usuário informa novo status de pedido' do
     #Assert
     expect(current_path).to eq order_path(order.id)
     expect(page).to have_content('Situação do Pedido: Cancelado')
+    expect(StockProduct.count).to eq 0
   end
 end
